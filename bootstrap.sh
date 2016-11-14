ba411a065bc122ed166119257a732ad8b616b9ae
Tomas Papan <tomas.papan@gmail.com>
2016-11-14 16:56:57 +0100

    Fix an issue in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,33 @@
-
-dfasaf
-asd#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master
+git pull -r origin master
 
 git submodule init
 git submodule update
 
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        --exclude "homebrew.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
+        -avh --no-perms . ~
 
     mkdir ~/.vim/backup > /dev/null 2>&1
     mkdir ~/.vim/tmp > /dev/null 2>&1
     mkdir ~/.vim/undodir > /dev/null 2>&1
     mkdir ~/go > /dev/null 2>&1
 
-	source ~/.bash_profile
+    source ~/.bash_profile
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 unset doIt
