6cdf9887b65a4094ee5a8dece13693a3d35fc9f0
Tomas Papan <tomas.papan@gmail.com>
2018-01-30 11:33:29 +0000

    Add support for profiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,26 +5,19 @@ cd "$(dirname "${BASH_SOURCE}")"
 git submodule init
 git submodule update --init --recursive
 
-
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-        --exclude "homebrew.sh" --exclude "README.md" --exclude .gitlab-ci.yml \
-        --exclude "LICENSE-MIT.txt" --exclude "test.sh" \
-        --exclude ".gitmodules" --exclude ".gitignore" \
-        --exclude "homebrew-cask.sh" \
-        -avh --no-perms . ~
 
-    mkdir ~/.vim/backup > /dev/null 2>&1
-    mkdir ~/.vim/tmp > /dev/null 2>&1
-    mkdir ~/.vim/undodir > /dev/null 2>&1
-    mkdir ~/go > /dev/null 2>&1
+    shopt -s dotglob
+
+    cd ./profiles/basic
+    for i in *; do
+        ln -sf "$PWD/$i" $HOME/
+    done
 
     if [ ! -f ~/.vim/autoload/plug.vim ]; then
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
-
-    source ~/.zshrc
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
