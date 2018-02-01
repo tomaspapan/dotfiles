125465ec9d91766162a22bfc1cc6647a2f1b2baa
Tomas Papan <tomas.papan@gmail.com>
2018-02-01 10:06:22 +0100

    Add better profile support
    Closes #1

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,60 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-git submodule init
-git submodule update --init --recursive
+FORCE=0
+EXECUTE=1
+PROFILE="basic git"
+
+function parseInput() {
+
+    while [[ $# -gt 0 ]]; do
+        positional=()
+        key=$1
+        case $key in
+            -f|--force)
+                FORCE=1
+                shift
+                ;;
+            -p|--profile)
+                PROFILE=$2
+                shift
+                shift
+                ;;
+            -l|--list)
+                listProfiles
+                EXECUTE=0
+                shift
+                ;;
+            *)
+                positional+=("$1")
+                shift
+                ;;
+        esac
+    done
+}
+
+function listProfiles() {
+    echo "*basic *git openbox ssh"
+    echo 
+    echo " * - the default profile"
+    echo "Profiles can be definied as -p"
+    echo 'example: ./boostrap -p "basic git ssh"'
+}
 
 function doIt() {
 
     shopt -s dotglob
 
-    cd ./profiles/basic
-    for i in *; do
-        ln -sf "$PWD/$i" $HOME/
+    cd ./profiles
+
+    for profile in $PROFILE; do
+        echo -n "Applying profile=$profile..."
+        cd ./$profile
+        for file in *; do
+            ln -sf "$PWD/$file" $HOME/
+        done
+        echo "done"
+        cd ..
     done
 
     if [ ! -f ~/.vim/autoload/plug.vim ]; then
@@ -20,13 +64,26 @@ function doIt() {
     fi
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-    echo ""
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
+function main() {
+    git submodule init
+    git submodule update --init --recursive
+
+    parseInput $@
+
+    if [ $EXECUTE -eq 1 ]; then
+        if [ $FORCE -eq 1 ]; then
+            doIt
+        else
+            read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+            echo ""
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                doIt
+            fi
+        fi
     fi
-fi
-unset doIt
+}
+
+# main
+
+main $@
+
